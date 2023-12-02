41090c2ac723017613e73e81f1a48a37b50a8846
mk10100 <mohamedkhalil10100@gmail.com>
2023-12-02 15:29:53 -0500

    first commit

---
+++ views/login.js
@@ -0,0 +1,19 @@
+function showSignUpForm() {
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("signupForm").style.display = "block";
+}
+
+function showLoginForm() {
+  document.getElementById("signupForm").style.display = "none";
+  document.getElementById("loginForm").style.display = "block";
+}
+
+function login() {
+  // Add your login logic here
+  alert("Login functionality will be implemented here.");
+}
+
+function signUp() {
+  // Add your sign-up logic here
+  alert("Sign Up functionality will be implemented here.");
+}
